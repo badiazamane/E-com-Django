379adec770158ceba221848734d2c68ee43ee52d
Badiazamane Benarab <benarabbadi05@gmail.com>
2023-04-17 12:21:23 +0200

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+python manage.py runserver
+python manage.py makemigrations
+python manage.py createsuperuser
+python manage.py migrate
+python           manage.py           startapp           badi
+django-admin startproject LOCALLIBRARY
+
---
+++ catalog/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
+
+    fieldsets = (
+        ('General Information', {
+            'fields': ('first_name', 'last_name')
+        }),
+        ('Dates', {
+            'fields': (('date_of_birth', 'date_of_death'))
+        }),
+    )
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
