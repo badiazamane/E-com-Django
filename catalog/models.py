1e4b9261fa0636a65fe65b0dd3ed3fe7413d4227
Badiazamane Benarab <benarabbadi05@gmail.com>
2023-04-24 21:25:13 +0200

    1

--- catalog/models.py
+++ catalog/models.py
@@ -5,6 +5,9 @@ import uuid  # Required for unique book instances
 from django.contrib.auth.models import User
 from datetime import date
 from django.core.validators import RegexValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
+
 
 # !Users modele
 
@@ -29,7 +32,7 @@ class User(models.Model):
 
 class Product(models.Model):
     """Model representing a product."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for users')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for Product')
     name = models.CharField(
         max_length=80, help_text='Enter the product name (e.g. book)')
     description = models.CharField(max_length=100)
@@ -37,6 +40,8 @@ class Product(models.Model):
     Seller_ID = models.ForeignKey('User', on_delete=models.RESTRICT)
     subCategories_ID = models.ForeignKey('subCategories', on_delete=models.RESTRICT)
     Categories_ID = models.ForeignKey('Categories', on_delete=models.RESTRICT)
+    created_date = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
         """String for representing the Model object."""
         return self.name
@@ -44,11 +49,11 @@ class Product(models.Model):
 
 class Order(models.Model):
     """Model representing a product."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for users')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for Order')
     Price = models.IntegerField(help_text='Enter the price')
-    date_of_birth = models.DateField(null=True, blank=True)
     buyer_ID = models.ForeignKey('User', on_delete=models.RESTRICT)
     product_ID = models.ForeignKey('Product', on_delete=models.RESTRICT)
+    created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         """String for representing the Model object."""
@@ -56,7 +61,7 @@ class Order(models.Model):
 
 class Categories(models.Model):
     """Model representing a product."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for users')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for Categories')
     name = models.CharField(
         max_length=80, help_text='Enter the category name (e.g. book)')
     description = models.CharField(max_length=100)
@@ -67,11 +72,24 @@ class Categories(models.Model):
 
 class subCategories(models.Model):
     """Model representing a product."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for users')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for subCategories')
     name = models.CharField(
         max_length=80, help_text='Enter the subcategory name (e.g. book)')
     description = models.CharField(max_length=100)
     Categories_ID = models.ForeignKey('Categories', on_delete=models.RESTRICT)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+class Review(models.Model):
+    """Model representing a product."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for review')
+    Rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.CharField(
+        max_length=200, help_text='comment')
+    created_date = models.DateTimeField(default=timezone.now)
+    buyer_ID = models.ForeignKey('User', on_delete=models.RESTRICT)
+    product_ID = models.ForeignKey('Product', on_delete=models.RESTRICT)
 
     def __str__(self):
         """String for representing the Model object."""
